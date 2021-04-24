82c66fda859a3403931ea3ab73c0309524407369
bigduk88 <hangdeuk@naver.com>
2021-04-24 13:55:18 +0900

    setup backend project

---
+++ src/main/java/sparta/enby/EnbyApplication.java
@@ -0,0 +1,13 @@
+package sparta.enby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/enby/EnbyApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.enby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
